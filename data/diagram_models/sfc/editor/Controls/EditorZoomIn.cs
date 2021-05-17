d8ad551880315525f72b56ffa385786fd5378dea
Marco Burri <26461040+Kersoph@users.noreply.github.com>
2021-05-17 18:27:47 +0200

    Using more zoom possibilities

--- data/diagram_models/sfc/editor/Controls/EditorZoomIn.cs
+++ data/diagram_models/sfc/editor/Controls/EditorZoomIn.cs
@@ -5,14 +5,24 @@ namespace Osls.SfcEditor.Controls
 {
     public class EditorZoomIn : Button
     {
+        #region ==================== Public Methods ====================
         public override void _Ready()
         {
             Connect("pressed", this, nameof(OnButtonPressed));
+        }
+        
+        public override void _UnhandledKeyInput(InputEventKey @event)
+        {
+            if (@event.IsActionPressed("ui_do_more"))
+            {
+                OnButtonPressed();
+            }
         }
         
         public void OnButtonPressed()
         {
             GetNode<SfcEditorNode>("../../..").Sfc2dEditorNode.ZoomIn();
         }
+        #endregion
     }
 }
