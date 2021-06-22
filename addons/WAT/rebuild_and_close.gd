5adb36fe57c425d0452f2df3245dd5508b44b039
Marco Burri <26461040+Kersoph@users.noreply.github.com>
2021-06-22 17:02:40 +0200

    Getting experimental with a custom godot editor script in the pipeline

---
+++ addons/WAT/rebuild_and_close.gd
@@ -0,0 +1,13 @@
+tool
+extends Label
+
+var a = 0.0
+
+func _process(delta):
+	a = a + delta
+	text = String(a)
+	if (a > 10):
+		print("Exiting reimport process...")
+		OS.set_exit_code(1)
+		get_tree().quit()
+	pass
