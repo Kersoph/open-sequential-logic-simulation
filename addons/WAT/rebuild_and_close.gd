3011df4d1c8abff13f354ed5db8125fa9783571b
Marco Burri <26461040+Kersoph@users.noreply.github.com>
2021-06-22 18:12:18 +0200

    Removing several tests

--- addons/WAT/rebuild_and_close.gd
+++ addons/WAT/rebuild_and_close.gd
@@ -1,22 +1,16 @@
 tool
 extends Label
 
-var a = 0.0
+var delay = 0.0
 
 func _ready() -> void:
-	print("Starting reimport process...")
 	push_warning("Starting reimport process...")
 
 func _process(delta) -> void:
-	a = a + delta
-	text = String(a)
-	if (a > 10):
-		print("Exiting reimport process...")
+	delay = delay + delta
+	text = String(delay)
+	if (delay > 10):
 		push_warning("Exiting reimport process...")
-		OS.set_exit_code(1)
+		OS.exit_code = 0
 		get_tree().quit()
 	pass
-
-static func initialize() -> void:
-	print("rebuild_and_close is ready")
-	push_warning("rebuild_and_close is ready.")
