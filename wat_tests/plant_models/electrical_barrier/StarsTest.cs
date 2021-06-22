63bde69a2852c217ad753f7a5d501a45c5f158a5
Marco Burri <26461040+Kersoph@users.noreply.github.com>
2021-06-22 14:11:31 +0200

    Added more tests

--- wat_tests/plant_models/electrical_barrier/StarsTest.cs
+++ wat_tests/plant_models/electrical_barrier/StarsTest.cs
@@ -25,5 +25,37 @@ namespace Tests.PlantModels.ElectricalBarrier
             string sfcPath = "res://wat_tests/plant_models/electrical_barrier/TwoStar.sfc";
             PlantModelRunner.Test(this, scenePath, sfcPath, 2, 6000);
         }
+        
+        [Test]
+        public void ZeroStars()
+        {
+            string scenePath = "res://data/plant_models/electrical_barrier/ElectricalBarrierTest.tscn";
+            string sfcPath = "res://wat_tests/plant_models/electrical_barrier/Empty.sfc";
+            PlantModelRunner.Test(this, scenePath, sfcPath, 0, 6000);
+        }
+        
+        [Test]
+        public void LightsThreeStars()
+        {
+            string scenePath = "res://data/plant_models/electrical_barrier/lights/LightsTest.tscn";
+            string sfcPath = "res://wat_tests/plant_models/electrical_barrier/LightsThreeStar.sfc";
+            PlantModelRunner.Test(this, scenePath, sfcPath, 3, 2900);
+        }
+        
+        [Test]
+        public void LightsOneStar()
+        {
+            string scenePath = "res://data/plant_models/electrical_barrier/lights/LightsTest.tscn";
+            string sfcPath = "res://wat_tests/plant_models/electrical_barrier/LightsOneStar.sfc";
+            PlantModelRunner.Test(this, scenePath, sfcPath, 1, 2900);
+        }
+        
+        [Test]
+        public void LightsZeroStars()
+        {
+            string scenePath = "res://data/plant_models/electrical_barrier/lights/LightsTest.tscn";
+            string sfcPath = "res://wat_tests/plant_models/electrical_barrier/Empty.sfc";
+            PlantModelRunner.Test(this, scenePath, sfcPath, 0, 6000);
+        }
     }
 }
